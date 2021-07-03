6f8fbbb6a08cd3ddd20e86843cb4263960e1d469
Bacenz <71555262+Bacenz@users.noreply.github.com>
2021-07-03 23:07:55 +0700

    Delete src directory

--- src/Bruh.java
+++
@@ -1,9 +0,0 @@
-public class Bruh {
-    public class Human{
-        private String name;
-        private int age;
-    }
-    public static void main(String[] args){
-        System.out.println("Bruh moment");
-    }
-}
